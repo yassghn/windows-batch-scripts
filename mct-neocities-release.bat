70441673107f734bb30d18ac3b509bb664b9e5ff
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-05-03 15:08:29 -0500

    mct neocities release

---
+++ mct-neocities-release.bat
@@ -0,0 +1,67 @@
+@echo off
+setlocal enabledelayedexpansion
+
+rem
+rem mdn-canvas-tutorial neocities release
+rem   - convert mjs files to js
+rem   - rename mjs imports to js
+
+echo:
+echo -------------------------------------
+echo mdn-canvas-tutorial neocities release
+echo -------------------------------------
+echo:
+
+rem check argument was provided
+if "%~1" == "" (
+	echo error: missing directory param
+	goto end
+)
+
+rem todo better directory checking, needs \ at the end
+
+rem get dispath from command args as absolute path
+set distPath=%~dp1
+
+rem set vars
+set mjsExt=.mjs
+set jsExt=.js
+
+rem echo
+echo using path: %distPath%
+echo:
+
+rem recursively iterate files
+echo iterating files...
+echo:
+
+for /R %distPath% %%f in (*%jsExt%, *%mjsExt%) do (
+	rem print file info
+	echo %%~nxf
+	echo drive: %%~df
+	echo dir: %%~pf
+	echo ext: %%~xf
+
+	rem check if file is mjs file
+	if %%~xf == !mjsExt! (
+		rem change file extension
+		echo renaming file to %%~nf!jsExt!
+		ren %%f %%~nf!jsExt!
+	)
+	rem replace text in file
+	echo replacing mjs extension in imports
+	set file=%%~dpnf!jsExt!
+	powershell -Command "(gc !file!) -replace '!mjsExt!', '!jsExt!' | Out-File -encoding ASCII !file!"
+
+	rem print spacer
+	echo -----------------------------
+)
+
+echo:
+echo done
+
+:end
+endlocal
+echo:
+pause
+exit 0
